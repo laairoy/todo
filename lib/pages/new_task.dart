c125dcff30c3975e2345cbdcca4df33a61201c46
emanuelguerke <emanuelguerkecode@gmail.com>
2022-03-31 23:55:08 -0300

    add FloatingActionButton inbox => new_task

--- lib/pages/new_task.dart
+++ lib/pages/new_task.dart
@@ -4,10 +4,10 @@ import 'package:todo/models/task_list.dart';
 import 'package:todo/repositories/task_list_repository.dart';
 
 class NewTask extends StatefulWidget {
-  final void Function() onSave;
-  NewTask({Key? key, required this.listItem, required this.onSave})
-      : super(key: key);
-  late Item listItem;
+  //final void Function() onSave;
+
+  NewTask({Key? key, required listItem}) : super(key: key);
+  //late Item listItem;
   @override
   State<NewTask> createState() => _NewTaskState();
 }
@@ -19,13 +19,10 @@ class _NewTaskState extends State<NewTask> {
   TextEditingController _noteController = TextEditingController();
   Widget build(BuildContext context) {
     //   final table = TaskListRepository.instance.table;
-    final table = TaskListRepository.instance.table
-        .where((element) => element.listId == widget.listItem.id)
-        .where((element) => element.finished == false)
-        .toList();
+    final table = TaskListRepository.instance.table;
     return Scaffold(
         appBar: AppBar(
-          title: Text(widget.listItem.name),
+          title: Text('Criar nova tarefa'),
         ),
         body: Padding(
           padding: const EdgeInsets.all(10.1),
@@ -55,8 +52,8 @@ class _NewTaskState extends State<NewTask> {
               ElevatedButton(
                   onPressed: () {
                     print(table[2].name);
-                    print(table[4].name);
-                    print(table[9].name);
+                    //   print(table[4].name);
+                    //    print(table[9].name);
                     table.add(TaskList(
                         name: _nameController.text,
                         note: _noteController.text,
